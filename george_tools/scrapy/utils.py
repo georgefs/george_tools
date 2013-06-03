084e0cf238d86364ad7f495607dc979afb749258
george <goblin_george@gmail.com>
2013-06-03 18:21:02 +0800

    auto reload

--- george_tools/scrapy/utils.py
+++ george_tools/scrapy/utils.py
@@ -1,5 +1,6 @@
-__all__ = ['set_job_info',]
-
+__all__ = ['set_job_info', 'close_job', 'open_spider']
+import urllib2, urllib
+import urlparse
 import os
 def set_job_info(spider_class):
     '''
@@ -26,3 +27,23 @@ def set_job_info(spider_class):
 
     spider_class.__init__ = __init__
     return spider_class
+
+def close_job(project, jobid, host="http://localhost:6800"):
+    url = urlparse.urljoin(host, 'cancel.json')
+    data = dict()
+    data['project'] = project
+    data['job'] = jobid
+
+    data_str = urllib.urlencode(data)
+    req = urllib2.Request(url, data_str)
+    urllib2.urlopen(req)
+
+def open_spider(project, spider, host="http://localhost:6800"):
+    url = urlparse.urljoin(host, 'schedule.json')
+    data = dict()
+    data['project'] = project
+    data['spider'] = spider
+
+    data_str = urllib.urlencode(data)
+    req = urllib2.Request(url, data_str)
+    urllib2.urlopen(req)
